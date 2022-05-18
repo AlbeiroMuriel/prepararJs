51ab51c0165e4e95ee35e8d9d66cca652634f0ae
Albeiro Muriel <j.albeiromuriel@gmail.com>
2022-05-17 23:10:36 -0500

    scope modificado

--- funciones/scope.js
+++ funciones/scope.js
@@ -9,3 +9,8 @@ scopeGlobal();
 
 // console.log("Nombre: " + name + "\nApellido: " + apellido);    
 //GENERA UN ERROR, porque apellido es una variable local
+
+// funcion anonima
+let a=5
+let b=10
+let resultado = (a,b) => a+b
